6cf3c2f5d3a10ebf31f0e3cfa9c98228e152db51
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-11-29 02:49:09 +0200

    s

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+diff = 1
+for diff in range(1):
+    print("zYxWvUtSrQpOnM{}KjIhGfEdCbA".format("l"), end="")
