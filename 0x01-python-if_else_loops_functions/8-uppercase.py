f612fc524fcea640228011210ee0678ee5e005f0
Deadin <alshapshere@gmail.com>
2023-11-29 00:04:28 +0000

    winningtime

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        upper_char = chr(ord(char) - 32) if ord('a') <= ord(char) <= ord('z') else char
-        print("{}".format(upper_char))
+        uc = chr(ord(char) - 32) if ord('a') <= ord(char) <= ord('z') else char
+        print("{}".format(uc))
