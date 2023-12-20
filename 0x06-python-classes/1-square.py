27d5c8229964dea073c0f15af34efc719a9cbbc1
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-12-20 02:08:18 +0200

    as

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Define a new class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """Represent a square class."""
 
     def __init__(self, size):
         """Initialize a new Square.
